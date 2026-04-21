b746f0c3ac9858b1938b7b444c69b2c7b9c7e4e6
Kyle Billing <kyleb1008@outlook.com>
2026-04-21 16:39:48 +0100

    Adding MaturableItem unit tests.

--- Csharp/GildedRoseTests/Domain/Inventory/Handlers/MaturableItemHandlerTests.cs
+++ Csharp/GildedRoseTests/Domain/Inventory/Handlers/MaturableItemHandlerTests.cs
@@ -8,6 +8,79 @@ namespace GildedRoseTests.Domain.Inventory.Handlers
 {
     public class MaturableItemHandlerTests
     {
-        
+        [Fact]
+        public void UpdateItemProperties_DecreaseSellInBy1()
+        {
+            Item item = new StandardItem
+            {
+                Name = "Maturable Item",
+                SellIn = 10,
+                Quality = 20
+            };
+
+            IOptions<ItemSettings> itemSettings = Options.Create(new ItemSettings
+            {
+                DefaultQualityIncrement = 1,
+                MaxQuality = 40
+            });
+
+            ItemStateService itemStateService = new ItemStateService(itemSettings);
+
+            MaturableItemHandler handler = new MaturableItemHandler(itemStateService);
+
+            handler.UpdateItemProperties(item);
+
+            Assert.Equal(9, item.SellIn);
+        }
+
+        [Fact]
+        public void UpdateItemProperties_IncreasesQualityBy1()
+        {
+            Item item = new StandardItem
+            {
+                Name = "Maturable Item",
+                SellIn = 5,
+                Quality = 20
+            };
+
+            IOptions<ItemSettings> itemSettings = Options.Create(new ItemSettings
+            {
+                DefaultQualityIncrement = 1,
+                MaxQuality = 40
+            });
+
+            ItemStateService itemStateService = new ItemStateService(itemSettings);
+
+            MaturableItemHandler handler = new MaturableItemHandler(itemStateService);
+
+            handler.UpdateItemProperties(item);
+
+            Assert.Equal(21, item.Quality);
+        }
+
+        [Fact]
+        public void UpdateItemProperties_DoesNotIncreaseQualityBeyondMaxValue()
+        {
+            Item item = new StandardItem
+            {
+                Name = "Maturable Item",
+                SellIn = 5,
+                Quality = 40
+            };
+
+            IOptions<ItemSettings> itemSettings = Options.Create(new ItemSettings
+            {
+                DefaultQualityIncrement = 1,
+                MaxQuality = 40
+            });
+
+            ItemStateService itemStateService = new ItemStateService(itemSettings);
+
+            MaturableItemHandler handler = new MaturableItemHandler(itemStateService);
+
+            handler.UpdateItemProperties(item);
+
+            Assert.Equal(40, item.Quality);
+        }
     }
 }
