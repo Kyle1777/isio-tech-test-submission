1d6bbe67f54752990072c7d09324a2d0d3561212
Kyle Billing <kyleb1008@outlook.com>
2026-04-21 17:33:26 +0100

    LegendaryItemHandler unit tests.

--- Csharp/GildedRoseTests/Domain/Inventory/Handlers/LegendaryItemHandlerTests.cs
+++ Csharp/GildedRoseTests/Domain/Inventory/Handlers/LegendaryItemHandlerTests.cs
@@ -8,6 +8,106 @@ namespace GildedRoseTests.Domain.Inventory.Handlers
 {
     public class LegendaryItemHandlerTests
     {
-        
+        [Fact]
+        public void UpdateItemProperties_EnsureSellInIsMinus1()
+        {
+            Item item = new StandardItem
+            {
+                Name = "Legendary Item",
+                SellIn = 34,
+                Quality = 20
+            };
+
+            IOptions<ItemSettings> itemSettings = Options.Create(new ItemSettings
+            {
+                DefaultQualityIncrement = 1,
+                MaxQuality = 40
+            });
+
+            ItemStateService itemStateService = new ItemStateService(itemSettings);
+
+            LegendaryItemHandler handler = new LegendaryItemHandler(itemStateService);
+
+            handler.UpdateItemProperties(item);
+
+            Assert.Equal(-1, item.SellIn);
+        }
+
+        [Fact]
+        public void UpdateItemProperties_DoesNotChangeWithinRangeQuality()
+        {
+            Item item = new StandardItem
+            {
+                Name = "Legendary Item",
+                SellIn = 34,
+                Quality = 20
+            };
+
+            IOptions<ItemSettings> itemSettings = Options.Create(new ItemSettings
+            {
+                DefaultQualityIncrement = 1,
+                MaxQuality = 40
+            });
+
+            ItemStateService itemStateService = new ItemStateService(itemSettings);
+
+            LegendaryItemHandler handler = new LegendaryItemHandler(itemStateService);
+
+            handler.UpdateItemProperties(item);
+
+            Assert.Equal(20, item.Quality);
+        }
+
+        // KRB: Handle edge cases where the quality has values outside the range before starting the program.
+        [Fact]
+        public void UpdateItemProperties_EnsureQualityIsWithinMax()
+        {
+            Item item = new StandardItem
+            {
+                Name = "Legendary Item",
+                SellIn = 34,
+                Quality = 49
+            };
+
+            IOptions<ItemSettings> itemSettings = Options.Create(new ItemSettings
+            {
+                DefaultQualityIncrement = 1,
+                MaxQuality = 40
+            });
+
+            ItemStateService itemStateService = new ItemStateService(itemSettings);
+
+            LegendaryItemHandler handler = new LegendaryItemHandler(itemStateService);
+
+            handler.UpdateItemProperties(item);
+
+            Assert.Equal(40, item.Quality);
+        }
+
+        // KRB: Handle edge cases where the quality has values outside the range before starting the program.
+        [Fact]
+        public void UpdateItemProperties_EnsureQualityIsWithinMin()
+        {
+            Item item = new StandardItem
+            {
+                Name = "Legendary Item",
+                SellIn = 34,
+                Quality = -30
+            };
+
+            IOptions<ItemSettings> itemSettings = Options.Create(new ItemSettings
+            {
+                DefaultQualityIncrement = 1,
+                MaxQuality = 40
+            });
+
+            ItemStateService itemStateService = new ItemStateService(itemSettings);
+
+            LegendaryItemHandler handler = new LegendaryItemHandler(itemStateService);
+
+            handler.UpdateItemProperties(item);
+
+            Assert.Equal(0, item.Quality);
+        }
     }
 }
